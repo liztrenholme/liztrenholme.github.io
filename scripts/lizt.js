fdd5b7e809f36c29acbe53de543398bad8256034
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-10-21 01:13:32 -0400

    rearranged projects, enhanced mobile view

--- scripts/lizt.js
+++ scripts/lizt.js
@@ -1,7 +1,9 @@
+// This allows navbar to collapse after cliking/tapping on menu item
 $(document).on('click','.navbar-collapse.show',function(e) {
     $(this).collapse('hide');
   });
 
+  // Allows AOS fadein animations to work on scroll
   AOS.init({
     duration: 1200,
   });
